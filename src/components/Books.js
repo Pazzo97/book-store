fa3f8f6db2c2c62861695dec2d654c0089dbd4f1
Pazzo97 <pazzomuk@gmail.com>
2022-06-07 21:00:42 +0200

    Use redux

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,30 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
 
 const Books = () => {
-  const books = [
-    {
-      title: '1000 Years of Joys and Sorrows',
-      author: 'Ai Weiwei',
-      id: 1,
-    },
-    {
-      title: 'Road to Memphis',
-      author: 'Mildred D. Taylor',
-      id: 2,
-    },
-    {
-      title: 'Silas Marner',
-      author: 'George Eliot',
-      id: 3,
-    },
-  ];
+  const books = useSelector((state) => state.books);
   return (
     <div>
-      {books.map((book) => (
-        <Book key={book.id} title={book.title} author={book.author} id={book.id} />
-      ))}
+      {books
+        && books.books.map((book) => (
+          <Book
+            key={book.id}
+            title={book.title}
+            author={book.author}
+            id={book.id}
+          />
+        ))}
       <AddBook />
     </div>
   );
